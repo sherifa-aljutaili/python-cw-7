0fda18c617ed67ffb878f1db80cc99446cd33775
sherifa-aljutaili <142884985+sherifa-aljutaili@users.noreply.github.com>
2023-09-16 12:30:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-#PT1
+
 class person :
-    name = 'sherifa'
-    age = 18 
+    def __int__ (self, name, age) :
+    self.name = name
+    self.age = age
+    if age > 18 :
+        print("too many responsibilities")
+    elif age < 18:
+        print("lucky")
+person("dana",15)
+print (person)
 
 
 
-
-hhbb
